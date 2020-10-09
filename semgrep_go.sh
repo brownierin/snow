0ed74990e7f48665530a492c3a796f4c2acaae6c
Erin Browning <ebrowning@slack-corp.com>
2020-10-09 12:16:49 -0700

    Add check for virtualenv folder, create if it doesn't exist

--- semgrep_go.sh
+++ semgrep_go.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-python -m pip3 install semgrep
+if [ ! -d env ]; then
+	python3 -m venv env
+fi
+
+source env/bin/activate
+
+# python --version
+# python3 --version
+# pip --version
+# pip3 --version
+
+python3 -m pip install -vvv semgrep --user
 
 repos=`cat enabled`
 
-for repo in $repos
-do
+for repo in $repos; do
 	outfile="results-$repo.json"
 	git_repo="git@slack-github.com:slack/$repo.git"
-	git clone $git_repo
-	semgrep --config=golang/semgrep.yaml --json -o artifacts/$outfile $repo
+	if [ ! -d $repo ]; then
+		git clone $git_repo
+	fi
+	semgrep --config=golang/semgrep.yaml --json -o $WORKSPACE/artifacts/$outfile $repo
 done
