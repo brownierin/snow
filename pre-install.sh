627bd4dd2af2a3a236014be25ddd681bd15a3020
Erin Browning <ebrowning@slack-corp.com>
2021-04-09 11:09:53 -0700

    put sudo in noniteractive mode

--- pre-install.sh
+++ pre-install.sh
@@ -5,7 +5,7 @@ set +x
 # fix ensurepip error
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
-sudo dpkg-reconfigure locales
+sudo -n dpkg-reconfigure locales
 
 [ ! -d "env" ] && python3 -m venv env
 source env/bin/activate
