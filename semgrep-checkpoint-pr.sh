121ea8ffe336e85eb88f0f040f1a569c90ad9b31
Tim Faraci <tfaraci@slack-corp.com>
2021-04-09 12:13:52 -0700

    add semgrep-checkpoint-pre.sh to run pre-install.sh

--- semgrep-checkpoint-pr.sh
+++ semgrep-checkpoint-pr.sh
@@ -4,6 +4,9 @@
 #This test resides within SNOW but is kicked off from Checkpoint here...
 #https://slack-github.com/slack/checkpoint/blob/master/src/config/ProdConf.hack
 
+#Run pre-install.sh as we need f-strings to work
+./pre-install.sh 
+
 #Cleanup old version of SNOW
 rm -rf ./snow
 
