8c8712bd8473a77978cb6a340ab99394cb9f7a09
Erin Browning <ebrowning@slack-corp.com>
2021-04-09 11:12:17 -0700

    third attempt

--- pre-install.sh
+++ pre-install.sh
@@ -2,11 +2,6 @@
 
 set +x
 
-# fix ensurepip error
-export LC_ALL="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
-sudo -n dpkg-reconfigure locales
-
-[ ! -d "env" ] && python3 -m venv env
+[ ! -d "env" ] && python3.5 -m venv env -
 source env/bin/activate
 pip3 install -r requirements.txt
