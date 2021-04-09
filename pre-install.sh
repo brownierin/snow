268e0159ecac9c723959e1b6aab1bf292c769085
Erin Browning <ebrowning@slack-corp.com>
2021-04-09 11:02:07 -0700

    Fix missing dependency

--- pre-install.sh
+++ pre-install.sh
@@ -2,6 +2,8 @@
 
 set +x
 
+# install python3-venv so ensurepip is available
+apt-get install python3-venv
 [ ! -d "env" ] && python3 -m venv env
 source env/bin/activate
 pip3 install -r requirements.txt
