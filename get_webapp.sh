28b385eac8813ff56d4f83c3b1ff38a70a24b777
Erin Browning <ebrowning@slack-corp.com>
2021-04-27 12:14:00 -0700

    update set, fix syntax

--- get_webapp.sh
+++ get_webapp.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-set +x
+set -x
 
 webapp_repo=$WORKSPACE/snow/repositories/webapp/
 webapp_url=git@slack-github.com:slack/webapp
 ref=/mnt/persist/https___slack_github_com_slack_webapp
 
-if [ ! -d webapp_repo ]
+if [ ! -d $webapp_repo ]
 then
     echo "[+] clone webapp from reference"
     git -C $WORKSPACE/snow/repositories clone --reference $ref $webapp_url
