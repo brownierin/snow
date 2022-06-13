9085a51afe87f2a2f744d340551760e93b07fef3
Erin Browning <ebrowning@slack-corp.com>
2022-06-13 15:18:19 -0700

    run black on config/message_text.py

--- config/message_text.py
+++ config/message_text.py
@@ -1,24 +1,22 @@
-
-
 banner = """
 :snowflake: *Daily Semgrep scan report* :snowflake:
 :blob-throw-snow-left: *Rules Triggered* :blob-throw-snow-right:
 """
 
 
-high_alert_text="""
+high_alert_text = """
 :fire: :fire: :fire: :fire: :fire: :fire:
 :fire::block-h: :block-i: :block-g: :block-h:  :fire:
 :fire: :fire: :fire: :fire: :fire: :fire:
 """
 
-normal_alert_text="""
+normal_alert_text = """
 :snowflake::snowflake::snowflake::snowflake::snowflake::snowflake::snowflake::snowflake::snowflake:
 :snowflake::block-n: :block-o: :block-r: :block-m: :block-a: :block-l::snowflake:
 :snowflake::snowflake::snowflake::snowflake::snowflake::snowflake::snowflake::snowflake::snowflake:
 """
 
-no_vulns_text="""
+no_vulns_text = """
 :black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square::black_square:
 :black_square::sun-turtle::black_square::black_square::black_square::sun-turtle::black_square::black_square::sun-turtle::sun-turtle::black_square::black_square::black_square::black_square::sun-turtle::black_square::black_square::black_square::sun-turtle::black_square::sun-turtle::black_square::black_square::black_square::sun-turtle::black_square::sun-turtle::black_square::black_square::black_square::sun-turtle::black_square::black_square::black_square::sun-turtle::black_square::black_square::sun-turtle::sun-turtle::sun-turtle::black_square:
 :black_square::sun-turtle::sun-turtle::black_square::black_square::sun-turtle::black_square::sun-turtle::black_square::black_square::sun-turtle::black_square::black_square::black_square::sun-turtle::black_square::black_square::black_square::sun-turtle::black_square::sun-turtle::black_square::black_square::black_square::sun-turtle::black_square::sun-turtle::black_square::black_square::black_square::sun-turtle::sun-turtle::black_square::black_square::sun-turtle::black_square::sun-turtle::black_square::black_square::black_square::black_square:
