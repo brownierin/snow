900dd7c3761b9fde976acd5d5313d6bd8036cc4c
Erin Browning <ebrowning@slack-corp.com>
2022-06-14 12:05:56 -0700

    Add tests for error handling

--- error_handling.py
+++ error_handling.py
@@ -10,7 +10,8 @@ class ErrorHandling:
     """
 
     def __init__(self, channel):
-        self.slack_sdk = slack.WebClient(token=os.environ.get("BOT_TOKEN"))
+        self.token = os.environ.get("BOT_TOKEN")
+        self.slack_sdk = slack.WebClient(token=self.token)
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.INFO)
         self.ts = None
@@ -24,18 +25,18 @@ class ErrorHandling:
         """
         self.find_or_create_thread()
         try:
-            self.slack_sdk.chat_postMessage(channel=self.channel, text=error, thread_ts=self.ts)
+            resp = self.slack_sdk.chat_postMessage(channel=self.channel, text=error, thread_ts=self.ts)
         except Exception as e:
             logging.warning(e)
             return
+        else:
+            return resp
 
     def find_or_create_thread(self):
         """
         This function finds or creates a thread.
         """
         if not self.ts:
-            # response = self.slack_sdk.conversations_open(channel=self.channel)
-            # self.ts = response["channel"]["thread_ts"]
             try:
                 result = self.slack_sdk.chat_postMessage(channel=self.channel, text=self.error_message)
                 self.ts = result["ts"]
---
+++ tests/unit/test_error_handling.py
@@ -0,0 +1,39 @@
+from error_handling import ErrorHandling
+import vcr
+import os
+import configparser
+
+
+SNOW_ROOT = os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + "/../../")
+config = configparser.ConfigParser()
+config.read(f"{SNOW_ROOT}/config/test.cfg")
+channel = config["general"]["channel"]
+
+
+def test_create_thread():
+    err = ErrorHandling(channel=channel)
+    assert err.token is not None
+    assert err.ts is None
+    with vcr.use_cassette(f"{SNOW_ROOT}/tests/unit/fixtures/error_handling_create_thread.yaml"):
+        err.find_or_create_thread()
+        assert err.ts is not None
+
+
+def test_find_thread():
+    err = ErrorHandling(channel=channel)
+    with vcr.use_cassette(f"{SNOW_ROOT}/tests/unit/fixtures/error_handling_create_thread.yaml"):
+        err.find_or_create_thread()  # creates thread
+        assert err.ts is not None
+        ts = err.ts
+        with vcr.use_cassette(f"{SNOW_ROOT}/tests/unit/fixtures/error_handling_find_thread.yaml"):
+            err.find_or_create_thread()  # finds thread
+        assert err.ts == ts
+
+
+def test_post_error():
+    err = ErrorHandling(channel=channel)
+    with vcr.use_cassette(f"{SNOW_ROOT}/tests/unit/fixtures/error_handling_create_thread.yaml"):
+        err.find_or_create_thread()  # creates thread
+        with vcr.use_cassette(f"{SNOW_ROOT}/tests/unit/fixtures/error_handling_post_error.yaml"):
+            resp = err.post_error("test")  # posts error
+            assert resp["ok"] is True
