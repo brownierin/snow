014920491d063bde2330d49c46c319b40d1b1e87
Olivier Arteau <oarteau@slack-corp.com>
2022-08-02 13:36:03 -0400

    Add a unit test to make sure we don't have missing false positive file.

--- tests/unit/test_content_integrity.py
+++ tests/unit/test_content_integrity.py
@@ -1,8 +1,12 @@
 import os
 import glob
 import json
+import sys
 
 SNOW_ROOT = os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + "/../../")
+
+sys.path.insert(0, SNOW_ROOT)
+from run_semgrep import get_repo_list, find_repo_language
 
 # Checks that all false positive file are valid
 def test_valid_false_positives_file():
@@ -14,3 +18,13 @@ def test_valid_false_positives_file():
         with open(file, "r") as f:
             data = json.load(f)
             assert isinstance(data, dict)
+
+# Checks that a false positive file exists for all enabled repo
+def test_no_missing_false_positive_file():
+    repos = get_repo_list()
+
+    for repo_long in repos:
+        language = find_repo_language(repo_long)
+        url, org, repo = repo_long.split("/")
+        fp_file = f"{SNOW_ROOT}/languages/{language}/false_positives/{repo}_false_positives.json"
+        assert os.path.exists(fp_file), f"'{fp_file}' is missing"
