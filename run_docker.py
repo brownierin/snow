1010fa87e98bf2ea3b6c861348cade7543f44a46
Erin Browning <ebrowning@slack-corp.com>
2023-02-12 18:50:09 -0800

    what to fix next

--- run_docker.py
+++ run_docker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import logging
 import os
+
 
 import src.verify as verify
 from src.util import *
@@ -9,18 +9,19 @@ from src.config import *
 import src.git as git
 
 
-# verify.get_docker_image()
-# verify.build_dockerfile()
-# verify.build_container()
+verify.get_docker_image()
+verify.build_dockerfile()
+verify.build_container()
 
-docker_base_string = f"docker run -t -v {SNOW_ROOT}:/src slack/semgrep poetry run /src/run_semgrep.py"
+results_dir = os.environ.get("CIBOT_ARTIFACT_DIR")
+docker_base_string = f"docker run -t -v {SNOW_ROOT}:/src -e CIBOT_ARTIFACT_DIR={results_dir} slack/semgrep poetry run /src/run_semgrep.py"
+
 
 # daily
 git.download_repos()
-run_long_command(f"{docker_base_string} -m daily")
+run_long_command(f"{docker_base_string} -m daily --no-cleanup")
 
 # pr
-# repo_long = "slack-github.com/slack/fake_repo"
-# git.git_ops_pr_scan(repo_long)
-# run_long_command(f"{docker_base_string} -m pr -r {repo_long}")
-
+repo_long = "slack-github.com/slack/fake_repo"
+git.git_ops_pr_scan(repo_long)
+run_long_command(f"{docker_base_string} -m pr -r {repo_long} --no-cleanup")
