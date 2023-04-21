ffa8a0506bafd119dc7dafee6e1d5b784291fb47
Erin Browning <ebrowning@slack-corp.com>
2023-04-20 22:44:16 -0700

    change poetry path for export

--- ci/test-core.sh
+++ ci/test-core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pip3 install -r requirements.txt
-export PATH=$HOME/.poetry/bin:$PATH
+export PATH="$HOME/.local/bin:$PATH"
 poetry install
 python3 -m pytest tests/unit/
--- ci/test-integration.sh
+++ ci/test-integration.sh
@@ -2,6 +2,6 @@
 
 python3 -m pip install wheel
 python3 -m pip install -r requirements.txt
-export PATH=$HOME/.poetry/bin:$PATH
+export PATH="$HOME/.local/bin:$PATH"
 poetry install
 python3 -m pytest tests/integration/
