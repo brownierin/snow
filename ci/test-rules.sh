f8e3b3f854d35a112029ee1cff4e5bef35669af2
Erin Browning <ebrowning@slack-corp.com>
2023-04-20 22:38:14 -0700

    export poetry path for test

--- ci/test-rules.sh
+++ ci/test-rules.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pip3 install -r requirements.txt
+export PATH=$HOME/.poetry/bin:$PATH
 poetry install
 
 poetry run $(pwd)/tests/rules/run_tests.py --generate_checkpoint_artifact --worker=4
